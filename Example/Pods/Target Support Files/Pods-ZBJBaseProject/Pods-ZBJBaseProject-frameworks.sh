05ed851d489e53c10cfc4c69cc024b492b163b4b
wb_zoubenjun <wb_zoubenjun@meituan.com>
2021-02-05 17:08:00 +0800

    pod install finish

--- Example/Pods/Target Support Files/Pods-ZBJBaseProject/Pods-ZBJBaseProject-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ZBJBaseProject/Pods-ZBJBaseProject-frameworks.sh
@@ -175,11 +175,23 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZBJBasicKit/ZBJBasicKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZBJHomeKit/ZBJHomeKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZBJPoplar/ZBJPoplar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZBJBasicKit/ZBJBasicKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZBJHomeKit/ZBJHomeKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ZBJPoplar/ZBJPoplar.framework"
